256be5b2293c3d2dff252990d2b6f96aba47ec1e
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2021-09-30 13:54:45 +1000

    add template parsing

---
+++ chapter1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println("executable " + os.Args[0])
+	fmt.Println("first argument " + os.Args[1])
+	name := os.Args[1]
+	htmlData := `
+	<html>
+	<head>
+	<title>
+	</title>
+	</head>
+	<body>
+	<h1>` + name + `</h1>
+	</body>
+	</html>
+	`
+	fileHTML, err := os.Create("index.html")
+
+	if err != nil {
+		log.Fatal("file not created ", err)
+	}
+	defer fileHTML.Close()
+
+	data := strings.NewReader(htmlData)
+
+	io.Copy(fileHTML, data)
+}
